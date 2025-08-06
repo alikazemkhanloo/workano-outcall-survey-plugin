67bd7b2820e6c9afc0cf8f0bae54da945d4b5cd0
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-08-06 19:49:06 +0330

    add a number to check for survey

--- wazo_agid/modules/did_outcall_set_features.py
+++ wazo_agid/modules/did_outcall_set_features.py
@@ -7,7 +7,7 @@ from wazo_agid import agid
 def did_outcall_set_features(agi, cursor, args):
     destination_number = agi.get_variable('WAZO_DSTNUM')
     print('destination_number', destination_number)
-    if destination_number =='09104719336':
+    if destination_number in ['09122066204', '09104719336']:
         agi.set_variable('WAZO_SURVEY_ENABLE', '1')
     
 # Register the AGI handler
