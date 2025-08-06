d15f7b22a25412cde3ee2c4cd8aa14f77ed2efa2
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-08-06 19:49:18 +0330

    /.

--- wazo_agid/modules/did_outcall_set_features.py
+++ wazo_agid/modules/did_outcall_set_features.py
@@ -6,7 +6,6 @@ from wazo_agid import agid
 
 def did_outcall_set_features(agi, cursor, args):
     destination_number = agi.get_variable('WAZO_DSTNUM')
-    print('destination_number', destination_number)
     if destination_number in ['09122066204', '09104719336']:
         agi.set_variable('WAZO_SURVEY_ENABLE', '1')
     
