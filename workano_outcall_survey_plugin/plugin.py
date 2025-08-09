32fe554b400f27be975fdea6cfac807be9da3b4f
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-08-09 14:54:04 +0330

    init db

--- workano_outcall_survey_plugin/plugin.py
+++ workano_outcall_survey_plugin/plugin.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 class Plugin:
     def load(self, dependencies):
         logger.info('workano survey plugin start loading')
-        # init_db(
-        #     'postgresql://asterisk:proformatique@localhost/asterisk?application_name=wazo-survey-plugin')
+        init_db(
+            'postgresql://asterisk:proformatique@localhost/asterisk?application_name=workano-outcall-survey-plugin')
         api = dependencies['api']
 
         # api.add_resource(
