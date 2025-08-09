0d9dd47934525be3d58eac32e598a7dadfdbcd18
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-08-09 19:20:09 +0330

    update persistor to use outcall_survey table

--- workano_outcall_survey_plugin/survey/persistor.py
+++ workano_outcall_survey_plugin/survey/persistor.py
@@ -1,9 +1,9 @@
 from xivo_dao.helpers.persistor import BasePersistor
 from xivo_dao.resources.utils.search import CriteriaBuilderMixin
-from .model import SurveyModel
+from .model import OutcallSurveyModel
 
 class OutcallSurveyPersistor(CriteriaBuilderMixin, BasePersistor):
-    _search_table = SurveyModel
+    _search_table = OutcallSurveyModel
 
     def __init__(self, session, survey_search, tenant_uuids=None):
         self.session = session
@@ -11,7 +11,7 @@ class OutcallSurveyPersistor(CriteriaBuilderMixin, BasePersistor):
         self.tenant_uuids = tenant_uuids
 
     def _find_query(self, criteria):
-        query = self.session.query(SurveyModel)
+        query = self.session.query(OutcallSurveyModel)
         return self.build_criteria(query, criteria)
 
     def _search_query(self):
